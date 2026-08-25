425491d412d96a5bbcafe0a4e19058f3cda45cf7
nsantos <nsantos.ks@gmail.com>
2026-08-25 00:37:02 -0400

    manejo de versiones de archivos criticos

--- js/config.js
+++ js/config.js
@@ -8,10 +8,6 @@
  */
 const CONFIG = {
   // Endpoint Web App oficial de Google Apps Script
-  //API_BASE_URL: "https://script.google.com/a/gammalielanalytics.com/macros/s/AKfycbw1Jp8qHewaxNsneLZGH9311bVK1W1PHtzzyQB9nSHq_gO3AKouEh60LlfK7pTDHHY/exec",
-
-  //API_BASE_URL: "https://script.google.com/a/voluntariadocdfvzla.org/macros/s/AKfycbwdlHZBmwsyUZwGR6qKJ3GT_hURRayjbuFgRUeyYkSPNVM8klquV9gG3QId5BiiwaeZ/exec",
-  
   API_BASE_URL: "https://script.google.com/a/voluntariadocdfvzla.org/macros/s/AKfycby8ApHdmMEtwrFjNUDsTrHY5622osDHmDmaSjK8Lg71MrXMJba8PGB9RdbH1BALwdJv/exec",
 
   // Clave secreta institucional para validación en servidor
