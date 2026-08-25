a29f2f491d7b95f1f72c93c3484f7656d4309272
nsantos <nsantos.ks@gmail.com>
2026-08-25 03:23:18 -0400

    Actualización de API

--- js/config.js
+++ js/config.js
@@ -8,7 +8,7 @@
  */
 const CONFIG = {
   // Endpoint Web App oficial de Google Apps Script
-  API_BASE_URL: "https://script.google.com/a/voluntariadocdfvzla.org/macros/s/AKfycby8ApHdmMEtwrFjNUDsTrHY5622osDHmDmaSjK8Lg71MrXMJba8PGB9RdbH1BALwdJv/exec",
+  API_BASE_URL: "https://script.google.com/a/voluntariadocdfvzla.org/macros/s/AKfycbz1YYs7llAN2HL3H6XxbRkmztsc-paY5tR4InZ_Dx8ucew5NmAFNdIu5KUp9hxTld6A/exec",
 
   // Clave secreta institucional para validación en servidor
   CLIENT_SECRET_KEY: "CDF_Vzla_2026_Secure_Key_#X9",
