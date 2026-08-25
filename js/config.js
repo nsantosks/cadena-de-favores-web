0ae2bed3e4ae23beab3cf29b75fd07347412a31b
nsantos <nsantos.ks@gmail.com>
2026-08-25 04:07:14 -0400

    Ventana de Timeout ampliada a 5min

--- js/config.js
+++ js/config.js
@@ -17,7 +17,7 @@ const CONFIG = {
   SESSION_KEY: "userProfile",
 
   // Tiempo límite de espera ampliado para operativas pesadas de Apps Script (ms)
-  TIMEOUT_MS: 90000
+  TIMEOUT_MS: 300000
 };
 
 /**
